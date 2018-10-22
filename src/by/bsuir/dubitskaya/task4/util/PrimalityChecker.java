7aa09ab5dfa92cebd524669b13d310c6c1746acf
Karalina Dubitskaya <dubitskaya.karalina@gmail.com>
2018-10-22 22:36:05 +0300

    Add tests to task #4

--- src/by/bsuir/dubitskaya/task4/util/PrimalityChecker.java
+++ src/by/bsuir/dubitskaya/task4/util/PrimalityChecker.java
@@ -2,7 +2,11 @@ package by.bsuir.dubitskaya.task4.util;
 
 public class PrimalityChecker {
     public static boolean isPrime(int number) {
-        if (number < 2) {
+        if (number < 1) {
+            throw new IllegalArgumentException("The argument must be a natural number");
+        }
+
+        if (number == 1) {
             return false;
         }
         for (int i = 2, max = (int) Math.round(Math.sqrt(number)); i <= max; i++) {
